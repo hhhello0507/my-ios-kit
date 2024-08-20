f125bd85e36429953b283e8f8fdb1eb112f913f7
hhhello <hhhello0507@dgsw.hs.kr>
2024-08-20 16:10:37 +0900

    RowView

---
+++ Sources/MyDesignSystem/Layout/RowView.swift
@@ -0,0 +1,60 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by hhhello0507 on 8/20/24.
+//
+
+import SwiftUI
+
+public struct RowView: View {
+    
+    private let title: String
+    private let description: String?
+    private let action: () -> Void
+    
+    public init(
+        _ title: String,
+        description: String? = nil,
+        action: @escaping () -> Void
+    ) {
+        self.title = title
+        self.description = description
+        self.action = action
+    }
+    
+    public var body: some View {
+        Button {
+            action()
+        } label: {
+            HStack(spacing: 8) {
+                Text(title)
+                    .myFont(.headlineM)
+                    .foreground(Colors.Label.normal)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                if let description {
+                    Text(description)
+                        .myFont(.bodyM)
+                        .foreground(Colors.Label.assistive)
+                }
+                Image(icon: Icons.Arrow.ExpandArrow)
+                    .resizable()
+                    .renderingMode(.template)
+                    .foreground(Colors.Label.alternative)
+                    .frame(width: 12, height: 12)
+                    .rotationEffect(.degrees(180))
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 8)
+            .background(Colors.Background.normal)
+        }
+        .applyAnimation()
+    }
+}
+
+#Preview {
+    RowView("Title", description: "Description") {
+        print("WOW")
+    }
+    .registerWanted()
+}
