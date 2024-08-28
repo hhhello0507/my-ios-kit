bb9c9edcb2705c6ae1603361fa9f29446acbb756
hhhello <hhhello0507@dgsw.hs.kr>
2024-08-28 11:24:47 +0900

    Create OperatorUtil

--- Sources/MyMoya/Endpoint.swift
+++ Sources/MyMoya/Endpoint.swift
@@ -38,12 +38,3 @@ public extension Endpoint {
         .successCodes
     }
 }
-
-infix operator - : AdditionPrecedence
-func - (method: Moya.Method, path: String) -> (Moya.Method, String) {
-    return (method, path)
-}
-
-func - (m: (Moya.Method, String), task: Moya.Task) -> (Moya.Method, String, Moya.Task) {
-    return (m.0, m.1, task)
-}
